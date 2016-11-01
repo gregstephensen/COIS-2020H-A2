469ca77cfa330a8397947e9951f86a76760fba4a
Josh H <josh.terance@gmail.com>
2016-10-31 21:17:26 -0400

    Update Coordinate.cs

--- Coordinate.cs
+++ Coordinate.cs
@@ -1,5 +1,31 @@
-// Coordinate class to be incorperated into Main
-public class Coordinate
-{
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
+namespace Coordinate
+{
+    // Coordinate class to be incorperated into Main
+    public class Coordinate
+    {
+        private int longitude;  // from 0-99
+        private int latitude;   // from 0-99
+
+        public Coordinate(int longitude, int latitude)
+        {
+
+        }
+
+        // From the object class
+        public override bool Equals(Object obj)
+        {
+
+        }
+
+        // From the object class
+        public override int GetHashCode()
+        {
+
+        }
+    }
 }
