c5c148709014983318854bd161eca84920f0ed10
Josh H <josh.terance@gmail.com>
2016-10-31 21:23:17 -0400

    Update City.cs

--- City.cs
+++ City.cs
@@ -1,1 +1,20 @@
+// City class to be incorporated into main (put "using City;" at the top of the file)
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace City
+{
+    public class City
+    {
+        private string name;
+        private int population;
+
+        public City (string name, int population)
+        {
+            // code here
+        }
+    }
+}
 
